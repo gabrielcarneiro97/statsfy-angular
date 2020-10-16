8154556d470aeb0511d8b17456832518c329ccee
Gabriel Carneiro <gabriel.carneiro.castro@gmail.com>
2020-10-16 13:38:54 -0300

    testando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,14 @@ import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { pt_BR } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import pt from '@angular/common/locales/pt';
+
+/* AntD */
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
 
 import { TesteComponent } from './teste/teste.component';
+import { RouterModule } from '@angular/router';
+import { Teste2Component } from './teste2/teste2.component';
 
 registerLocaleData(pt);
 
@@ -20,14 +25,26 @@ registerLocaleData(pt);
   declarations: [
     AppComponent,
     TesteComponent,
+    Teste2Component,
   ],
   imports: [
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: TesteComponent,
+      },
+      {
+        path: 'teste',
+        component: Teste2Component
+      }
+    ]),
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
     NzButtonModule,
+    NzLayoutModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: pt_BR }],
   bootstrap: [AppComponent],
---
+++ src/app/teste2/teste2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teste2',
+  templateUrl: './teste2.component.html',
+  styleUrls: ['./teste2.component.scss']
+})
+export class Teste2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
